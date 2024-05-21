42094ce7daa93b562c4f49bef346896999830c41
lxxsxynnn <blip0819@gmail.com>
2024-05-21 11:11:02 +0900

    첫번째 커밋

---
+++ src/main/java/org/workat/workat_project/WorkatProjectApplication.java
@@ -0,0 +1,13 @@
+package org.workat.workat_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkatProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkatProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/workat/workat_project/WorkatProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.workat.workat_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkatProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
