7670f0f88f8c47cff25830de8a64636e65999cb0
DevKDuck <loading95@naver.com>
2024-05-21 14:06:45 +0900

    깃테스트2

--- src/main/java/soyeon/Hello.java
+++ src/main/java/soyeon/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("테스트테스트테스트테스트");
+        System.out.println("테스트2");
         
     }
 }
