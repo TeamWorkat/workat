8a0855b1d9dd33a349f13a76d7821c0e84d5ffa1
lxxsxynnn <blip0819@gmail.com>
2024-05-29 21:19:09 +0900

    사용자 상세 정보 조회 기능 구현

--- src/main/java/org/workat/workat_project/user/service/UserServiceImpl.java
+++ src/main/java/org/workat/workat_project/user/service/UserServiceImpl.java
@@ -2,6 +2,7 @@ package org.workat.workat_project.user.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.workat.workat_project.user.entity.UserDetailDTO;
 import org.workat.workat_project.user.entity.UserListDTO;
 import org.workat.workat_project.user.repository.UserMapper;
 
@@ -16,5 +17,10 @@ public class UserServiceImpl implements UserService {
     @Override
     public List<UserListDTO> getList() {
         return userMapper.getUserList();
+    }
+
+    @Override
+    public UserDetailDTO getUserDetail(int user_id) {
+        return userMapper.getUserDetail(user_id);
     }
 }
