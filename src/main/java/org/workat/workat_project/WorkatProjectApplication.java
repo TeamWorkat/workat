b1fd73284bd0a6bdb327c94c676119645da7fbf4
lxxsxynnn <158380091+lxxsxynnn@users.noreply.github.com>
2024-05-29 10:35:30 +0900

    Revert "안윤빈3"

--- src/main/java/org/workat/workat_project/WorkatProjectApplication.java
+++ src/main/java/org/workat/workat_project/WorkatProjectApplication.java
@@ -1,11 +1,9 @@
 package org.workat.workat_project;
 
-import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class WorkatProjectApplication {
 
     public static void main(String[] args) {
--- src/main/java/org/workat/workat_project/review/service/ReviewService.java
+++
@@ -1,9 +0,0 @@
-package org.workat.workat_project.review.service;
-
-import org.workat.workat_project.review.entity.ReviewResDTO;
-
-import java.util.List;
-
-public interface ReviewService {
-    List<ReviewResDTO> getReviewInfoList(int placeId);
-}
