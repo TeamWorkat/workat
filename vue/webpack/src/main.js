28674a7385cf1afa973b6a01c4a1c1928bd49c72
lxxsxynnn <blip0819@gmail.com>
2024-05-29 09:53:51 +0900

    Vue bootstrap 추가

--- vue/webpack/src/main.js
+++ vue/webpack/src/main.js
@@ -1,7 +1,11 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css';
+import BootstrapVueNext from 'bootstrap-vue-next';
 
 createApp(App)
     .use(router)
+    .use(BootstrapVueNext)
     .mount('#app');
