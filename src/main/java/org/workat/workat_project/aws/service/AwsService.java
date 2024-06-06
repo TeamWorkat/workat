04ad998e9267fadee86cfa8b26b5dcf03ff4071d
anyoonbin <yb226@kakao.com>
2024-06-06 16:25:19 +0900

    aws s3연동 시큐리티 url 살짝 수정

---
+++ src/main/java/org/workat/workat_project/aws/service/AwsService.java
@@ -0,0 +1,15 @@
+package org.workat.workat_project.aws.service;
+
+    import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileNotFoundException;
+
+public interface AwsService {
+    String getFileFolder(String fileFolder);
+
+    String uploadFile(MultipartFile file, String fileFolder);
+
+    String getFileUrl(String fileName);
+
+    void deleteFile(String fileName);
+}
