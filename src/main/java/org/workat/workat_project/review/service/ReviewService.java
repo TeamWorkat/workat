38fe20dab3c91da53f05a490c40f0c03ab533069
anyoonbin <yb226@kakao.com>
2024-06-08 17:14:10 +0900

    마이페이지 후기 리스트 추가

--- src/main/java/org/workat/workat_project/review/service/ReviewService.java
+++ src/main/java/org/workat/workat_project/review/service/ReviewService.java
@@ -1,9 +1,12 @@
 package org.workat.workat_project.review.service;
 
+import org.workat.workat_project.review.entity.ReviewListDTO;
 import org.workat.workat_project.review.entity.ReviewResDTO;
 
 import java.util.List;
 
 public interface ReviewService {
     List<ReviewResDTO> getReviewInfoList(int placeId);
+
+    List<ReviewListDTO> userReviewList(String mail);
 }
--- vue/webpack/src/views/UserDetail.vue
+++ vue/webpack/src/views/UserDetail.vue
@@ -20,6 +20,7 @@
 <script>
 import axios from 'axios';
 
+
 export default {
   name: 'UserDetail',
   data() {
